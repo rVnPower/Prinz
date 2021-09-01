17fd566d4e0367fd927affe7c22a025b555c4052
rVnPower <vonguyenvinguyen8888@gmail.com>
2021-09-01 10:53:10 +0700

    Polls update

--- cogs/doujin.py
+++ cogs/doujin.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands, tasks
 import asyncio
 from NHentai.nhentai import NHentai
+from NHentai.nhentai_async import NHentaiAsync 
 #####################################################
 
 class Doujin(commands.Cog): 
@@ -10,17 +11,60 @@ class Doujin(commands.Cog):
         self.bot = bot
 
     @commands.command(aliases=['rD'], description="Get a random doujin on NHentai")
-    async def random_doujin(self ,ctx):
+    async def random_doujin(self, ctx):
         if ctx.channel.is_nsfw():
-            nhentai = NHentai()
-            Doujin = nhentai.get_random()
-            embed = discord.Embed(colour=discord.Colour.blurple(), title=Doujin.title, url=f'https://nhentai.net/g/{Doujin.id}')
+            nhentai_async = NHentaiAsync()
+            Doujin = await nhentai_async.get_random()
+            embed = discord.Embed(colour=discord.Colour.blurple(), title=Doujin.title, url=f'https://nhentai.net/g/{Doujin.id}', description=f"ID: {Doujin.id}")
             embed.set_image(url=Doujin.images[0])
             await ctx.send(embed=embed)
         else:
             embed = discord.Embed(colour=discord.Colour.blurple())
             embed.set_author(name='You can only use this command in a NSFW channel!')
             await ctx.send(embed=embed)
+
+    @commands.command(description="Search for doujin on NHentai")
+    async def search_doujin(self, ctx, *, words:int):
+        current = 0
+        nhentai = NHentai()
+        SearchPage = nhentai.search(query='naruto', sort='popular', page=1)
+        Doujin = SearchPage.doujins[0]
+        embed = discord.Embed(color=discord.colour.blurple(), title=Doujin.title, url=f'https://nhentai.net/g/{Doujin.id}')
+        embed.set_image(url=Doujin.cover)
+
+        # Start looping
+        current = 0
+        # Add reactions and do stuff
+        buttons = ['⏪', '◀️', '▶️', '⏩']
+        msg = await ctx.send(embed=embeds[current])
+        for button in buttons:
+            await msg.add_reaction(button)
+        while True:
+            try:
+                reaction, user = await self.bot.wait_for("reaction_add", check=lambda reaction , user: user==ctx.author and reaction.emoji in buttons, timeout=60.0)
+            except asyncio.TimeoutError:
+                await ctx.send("Time out!")
+                return
+            else:
+                previous_page = current
+                if reaction.emoji == '⏪':
+                    current = 0
+                elif reaction.emoji == '◀️':
+                    if current > 0:
+                        current -= 1
+                    else:
+                        current = len(embeds) - 1
+                elif reaction.emoji == '▶️':
+                    if current < len(embeds):
+                        current += 1
+                    else:
+                        current = 0
+                elif reaction.emoji == '⏩':
+                    current = len(embeds) - 1
+                for button in buttons:
+                    await msg.remove_reaction(button, ctx.author)
+                    if current != previous_page:
+                        await msg.edit(embed=embeds[current])
 
     @commands.command(description="Read a doujin on NHentai")
     async def read_doujin(self, ctx, *, words:int):
--- cogs/tools.py
+++ cogs/tools.py
@@ -11,6 +11,10 @@ from core.chat_formatting import bold
 import asyncio
 #####################################################
 
+def to_emoji(c):
+    base = 0x1f1e6
+    return chr(base + c)
+
 class Tools(commands.Cog, description="Tools"):
     def __init__(self, bot):
         self.bot = bot
@@ -35,6 +39,67 @@ class Tools(commands.Cog, description="Tools"):
         embed.set_author(name=ctx.author)
         embed.set_image(url=ctx.author.avatar_url)
         await ctx.send(embed=embed)
+
+    @commands.command(description="Create a simple poll")
+    async def poll(self, ctx, *, time:int= 600, topic:str):
+        embed = discord.Embed(title=f"{ctx.author} asks: {topic}", description=f":one: Yes\n:two: No", color = discord.Colour.blurple())
+        embed.set_footer(text= f"Poll created at {ctx.message.created_at}", icon_url=ctx.author.avatar_url)
+        msg = await ctx.send(embed=embed)
+        try:
+            await ctx.message.delete()
+        except:
+            pass
+        await msg.add_reaction('1️⃣')
+        await msg.add_reaction('2️⃣')
+        
+
+        await asyncio.sleep(600)
+
+        newMSG = await ctx.fetch_message(msg.id)
+        onechoice = await newMSG.reactions[0].users().flatten()
+        secchoice = await newMSG.reactions[1].users().flatten()
+
+        result = "Tie"
+        if len(onechoice) > len(secchoice):
+            result = 'Yes'
+        elif len(secchoice) > len(onechoice):
+            result = 'No'
+
+        embed = discord.Embed(title= topic, description=f"Result: {result}", color = discord.Colour.blurple())
+        await newMSG.edit(embed=embed)
+
+    @commands.command()
+    @commands.guild_only()
+    async def quickpoll(self, ctx, *questions_and_choices: str):
+        """Makes a poll quickly.
+        The first argument is the question and the rest are the choices.
+        """
+
+        if len(questions_and_choices) < 3:
+            embed = discord.Embed(colour=discord.Colour.blurple(), description="Need at least 1 question with 2 choices.")
+            return await ctx.send(embed=embed)
+        elif len(questions_and_choices) > 21:
+            embed = discord.Embed(colour=discord.Colour.blurple(), description="You can only have up to 20 choices.")
+            return await ctx.send(embed=embed)
+
+        perms = ctx.channel.permissions_for(ctx.me)
+        if not (perms.read_message_history or perms.add_reactions):
+            return await ctx.send('Need Read Message History and Add Reactions permissions.')
+
+        question = questions_and_choices[0]
+        choices = [(to_emoji(e), v) for e, v in enumerate(questions_and_choices[1:])]
+
+        try:
+            await ctx.message.delete()
+        except:
+            pass
+
+        body = "\n".join(f"{key}: {c}" for key, c in choices)
+        embed = discord.Embed(colour=discord.Colour.blurple(), title=f"{ctx.author} asks: {question}\n\n", description=body)
+        embed.set_footer(text= f"Poll created at {ctx.message.created_at}", icon_url=ctx.author.avatar_url)
+        poll = await ctx.send(embed=embed)
+        for emoji, _ in choices:
+            await poll.add_reaction(emoji)
 
 def setup(bot):
     bot.add_cog(Tools(bot))
